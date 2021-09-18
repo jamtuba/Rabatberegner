fb840e013e92641782f3e0100d7c493d5e02549f
Janus Mogensen <jamtuba@gmail.com>
2021-09-18 18:19:37 +0200

    Initial commit

---
+++ Rabatberegner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rabatberegner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Rabatberegner/RabatBeregnerClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabatberegner
+{
+    public class RabatBeregnerClass
+    {
+        public double GetPrisEfterRabat(int antalVarer, double stykPris)
+        {
+            double bruttoPris = antalVarer * stykPris;
+            if (bruttoPris > 1000 && antalVarer > 10)
+            {
+                // Indkøb over 1000 kr. og flere end 10 varar giver 5% rabat
+                double rabat = bruttoPris * 0.05;
+                bruttoPris -= rabat;
+            }
+            else if (bruttoPris > 1000)
+            {
+                // Indkøb over 1000 kr. giver 3% rabat
+                double rabat = bruttoPris * 0.03;
+                bruttoPris = bruttoPris - rabat;
+            }
+            else if (antalVarer > 10)
+            {
+                // Flere end 10 varer giver 2% rabat
+                double rabat = bruttoPris * 0.02;
+                bruttoPris = bruttoPris - rabat;
+            }
+
+            return bruttoPris;
+        }
+
+    }
+}
